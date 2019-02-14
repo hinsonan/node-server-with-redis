578a2507cb81be5fc415b96abcd6568186b6c84a
developer <developer@localhost.localdomain>
2019-02-14 14:04:46 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+//********************************/
+// PROTOBUF CODE
+//********************************/
+var message = require("./test_message_pb.js")
+message.name.setName("Jack")
+console.log(message.name)
+
+//********************************/
+// REDIS CODE
+//********************************/
+var redis = require('redis');
+var client = redis.createClient('6379', '127.0.0.1');
+//this variable will hold the result of the client.get return value
+var foo
+
+client.on('connect', function() {
+    console.log('Redis client connected');
+});
+
+client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
+
+client.set('my test key', 'my test value', redis.print);
+client.get('my test key', function (error, result) {
+    if (error) {
+        console.log(error);
+        throw error;
+    }
+    foo = result
+    console.log('GET result ->' + result);
+});
+
+/*********************/
+// NODE SERVER CODE
+/*********************/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function(req, res){
+    
+    res.sendFile(__dirname + "/" + "index.html")
+    
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
